37a3523199e99dd63c2d3c3991dd2006415fee1d
Retrider <a55155@alunos.ipb.pt>
2024-06-23 04:21:38 +0100

    Lesson 3.2 - Make the World Whiz By

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 3;
+    private PlayerController playerControllerScript;
+    private float leftBound = -10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playerControllerScript.gameOver == false)
         transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle")) Destroy(gameObject);
     }
 }
